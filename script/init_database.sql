52cc7fbc827d7254f50606cd846474c9cd59a4b6
Hossam Nady Mohammed <nadyh7579@gmail.com>
2025-09-26 15:19:27 +0300

    Update init_database.sql

--- script/init_database.sql
+++ script/init_database.sql
@@ -1,13 +1,20 @@
-----INIT DATABASE -----------
-CREATE DATABASE GUEST ;
-GO 
+------------------------------------------------------------
+-- 1. INIT DATABASE
+------------------------------------------------------------
+CREATE DATABASE GUEST;
+GO
 
-USE GUEST 
-GO 
------------INIT SCHEMA --------
+USE GUEST;
+GO
+
+------------------------------------------------------------
+-- 2. INIT SCHEMAS
+------------------------------------------------------------
 CREATE SCHEMA SALES;
 GO
+
 CREATE SCHEMA FINANCE;
 GO
+
 CREATE SCHEMA HR;
 GO
